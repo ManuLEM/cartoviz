7830b590954f98e7be733b640d6d90ec01ecd7ad
Manuel Lemaire <2012manuell@psi.kiev.ua>
2014-03-26 09:58:58 +0100

    réorganisation et réindentation

--- script/main.js
+++ script/main.js
@@ -2,65 +2,65 @@ function initMap(position, zoom){
 	var zoom = zoom || 15;
 	var map;
 	require([
-	"esri/map", "esri/layers/ArcGISDynamicMapServiceLayer", 
-	"esri/TimeExtent", "esri/dijit/TimeSlider",
-	"dojo/_base/array", "dojo/dom", "dojo/domReady!"
+		"esri/map", "esri/layers/ArcGISDynamicMapServiceLayer", 
+		"esri/TimeExtent", "esri/dijit/TimeSlider",
+		"dojo/_base/array", "dojo/dom", "dojo/domReady!"
 	], function(
-	Map, ArcGISDynamicMapServiceLayer, 
-	TimeExtent, TimeSlider,
-	arrayUtils, dom
+		Map, ArcGISDynamicMapServiceLayer, 
+		TimeExtent, TimeSlider,
+		arrayUtils, dom
 	) {
-	map = new Map("mapDiv", {
-	  basemap: "streets",
-	  center: [position.coords.longitude, position.coords.latitude],
-	  zoom: zoom
-	});
+		map = new Map("mapDiv", {
+			basemap: "streets",
+			center: [position.coords.longitude, position.coords.latitude],
+			zoom: zoom
+		});
 
-	var opLayer = new ArcGISDynamicMapServiceLayer("http://sampleserver3.arcgisonline.com/ArcGIS/rest/services/Petroleum/KSWells/MapServer");
-	opLayer.setVisibleLayers([0]);
+		var opLayer = new ArcGISDynamicMapServiceLayer("http://sampleserver3.arcgisonline.com/ArcGIS/rest/services/Petroleum/KSWells/MapServer");
+		opLayer.setVisibleLayers([0]);
 
-	//apply a definition expression so only some features are shown 
-	var layerDefinitions = [];
-	layerDefinitions[0] = "FIELD_KID=1000148164";
-	opLayer.setLayerDefinitions(layerDefinitions);
+		//apply a definition expression so only some features are shown 
+		var layerDefinitions = [];
+		layerDefinitions[0] = "FIELD_KID=1000148164";
+		opLayer.setLayerDefinitions(layerDefinitions);
 
-	//add the gas fields layer to the map 
-	map.addLayers([opLayer]);
+		//add the gas fields layer to the map 
+		map.addLayers([opLayer]);
 
-	map.on("layers-add-result", initSlider);
+		map.on("layers-add-result", initSlider);
 
-	function initSlider() {
-	  var timeSlider = new TimeSlider({
-	    style: "width: 100%;"
-	  }, dom.byId("timeSliderDiv"));
-	  map.setTimeSlider(timeSlider);
-	  
-	  var timeExtent = new TimeExtent();
-	  timeExtent.startTime = new Date("1/1/1921 UTC");
-	  timeExtent.endTime = new Date("12/31/2009 UTC");
-	  timeSlider.setThumbCount(2);
-	  timeSlider.createTimeStopsByTimeInterval(timeExtent, 2, "esriTimeUnitsYears");
-	  timeSlider.setThumbIndexes([0,1]);
-	  timeSlider.setThumbMovingRate(2000);
-	  timeSlider.startup();
-	  
-	  //add labels for every other time stop
-	  var labels = arrayUtils.map(timeSlider.timeStops, function(timeStop, i) { 
-	    if ( i % 2 === 0 ) {
-	      return timeStop.getUTCFullYear(); 
-	    } else {
-	      return "";
-	    }
-	  }); 
-	  
-	  timeSlider.setLabels(labels);
-	  
-	  timeSlider.on("time-extent-change", function(evt) {
-	    var startValString = evt.startTime.getUTCFullYear();
-	    var endValString = evt.endTime.getUTCFullYear();
-	    dom.byId("daterange").innerHTML = "<i>" + startValString + " and " + endValString  + "<\/i>";
-	  });
-	}
+		function initSlider() {
+			var timeSlider = new TimeSlider({
+				style: "width: 100%;"
+			}, dom.byId("timeSliderDiv"));
+			map.setTimeSlider(timeSlider);
+
+			var timeExtent = new TimeExtent();
+			timeExtent.startTime = new Date("1/1/1921 UTC");
+			timeExtent.endTime = new Date("12/31/2009 UTC");
+			timeSlider.setThumbCount(2);
+			timeSlider.createTimeStopsByTimeInterval(timeExtent, 2, "esriTimeUnitsYears");
+			timeSlider.setThumbIndexes([0,1]);
+			timeSlider.setThumbMovingRate(2000);
+			timeSlider.startup();
+
+			//add labels for every other time stop
+			var labels = arrayUtils.map(timeSlider.timeStops, function(timeStop, i) { 
+				if ( i % 2 === 0 ) {
+					return timeStop.getUTCFullYear(); 
+				} else {
+					return "";
+				}
+			}); 
+
+			timeSlider.setLabels(labels);
+
+			timeSlider.on("time-extent-change", function(evt) {
+				var startValString = evt.startTime.getUTCFullYear();
+				var endValString = evt.endTime.getUTCFullYear();
+				dom.byId("daterange").innerHTML = "<i>" + startValString + " and " + endValString  + "<\/i>";
+			});
+		}
 	});
 }
 
