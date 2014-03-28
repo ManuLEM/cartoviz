22adf9ad4961a4aff4211d3b5a6e5b029f64ce7d
Manuel Lemaire <2012manuell@psi.kiev.ua>
2014-03-28 17:44:02 +0100

    arrondissements zoom on click, style change and limit to Paris

--- script/main.js
+++ script/main.js
@@ -1,19 +1,46 @@
 var map = L.map('map').setView([48.8567, 2.3508], 13);
 
-L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
-}).addTo(map);
-
-var myLayer = L.geoJson().addTo(map);
-$.getJSON("./data/arrondissements.geojson", function(collection) {
-	myLayer.addData(collection);
-});
-
-
 var southWest = new L.latLng([48.81, 2.22], map.getMaxZoom());
 var northEast = new L.latLng([48.91, 2.48], map.getMaxZoom());
 map.setMaxBounds(new L.LatLngBounds(southWest, northEast));
 
+L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community',
+	minZoom: 13
+}).addTo(map);
+
+var myLayer = L.geoJson().addTo(map);
+
+$.getJSON("./data/arrondissements.geojson", function(collection) {
+	var geojson = L.geoJson(collection, {
+		onEachFeature: onEachFeature,
+		style: {
+	        fillColor: "#ff7800",
+		    color: "red",
+		    weight: 1,
+		    opacity: 1,
+		    fillOpacity: 0.3
+	    }
+	}).addTo(map);
+
+	function clickFeature(e) {
+		var layer = e.target;
+		map.fitBounds(layer.getBounds());
+		//console.log(layer.feature.properties.name); //country info from geojson
+	}
+
+	function onEachFeature(feature, layer) {
+		layer.on({
+		  click: clickFeature
+		});
+	}
+
+	myLayer.on({
+		click: function() {
+			map.setView([0,0],8);
+		}
+	});
+});
 
 var popup = L.popup();
 
@@ -23,5 +50,4 @@ function onMapClick(e) {
         .setContent("You clicked the map at " + e.latlng.toString())
         .openOn(map);
 }
-
 map.on('click', onMapClick);
