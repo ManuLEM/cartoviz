61834bbb8aa2b38e0f9fd85b11a70ca0cfa47024
Manuel Lemaire <2012manuell@psi.kiev.ua>
2014-04-02 02:31:23 +0200

    added data, age variable in place

--- script/main.js
+++ script/main.js
@@ -117,6 +117,7 @@ var number = 0;
 var prev_feature = false;
 var prev_color = false;
 var data = {};
+var age_user = 20;
 
 function pointIsInPoly(p, polygon) {
     var isInside = false;
@@ -130,14 +131,14 @@ function pointIsInPoly(p, polygon) {
         maxY = Math.max(q[1], maxY);
     }
 
-    if (p[1] < minX || p[1] > maxX || p[0] < minY || p[0] > maxY) {
+    if (p[0] < minX || p[0] > maxX || p[1] < minY || p[1] > maxY) {
         return false;
     }
 
     var i = 0, j = polygon.length - 1;
     for (i, j; i < polygon.length; j = i++) {
-        if ( (polygon[i][1] > p[0]) != (polygon[j][1] > p[0]) &&
-                p[1] < (polygon[j][0] - polygon[i][1]) * (p[0] - polygon[i][1]) / (polygon[j][0] - polygon[i][1]) + polygon[i][0] ) {
+        if ( (polygon[i][1] > p[1]) != (polygon[j][1] > p[1]) &&
+                p[0] < (polygon[j][0] - polygon[i][1]) * (p[1] - polygon[i][1]) / (polygon[j][0] - polygon[i][1]) + polygon[i][0] ) {
             isInside = !isInside;
         }
     }
@@ -146,10 +147,14 @@ function pointIsInPoly(p, polygon) {
 }
 
 function getColor(feature) {
-    return feature.addedData.elements > 20   ? '#006064' :
-           feature.addedData.elements > 10   ? '#1D7074' :
-           feature.addedData.elements > 5    ? '#01939A' :
-                      						   '#34C6CD';
+    return feature.addedData.elements > 20   ? '#095793' :
+           feature.addedData.elements > 17   ? '#0b61a4' :
+           feature.addedData.elements > 13   ? '#2370ad' :
+           feature.addedData.elements > 10   ? '#3b80b6' :
+           feature.addedData.elements > 7    ? '#5490bf' :
+           feature.addedData.elements > 4    ? '#6ca0c8' :
+           feature.addedData.elements > 2    ? '#85b0d1' :
+                      						   '#9dbfda';
 }
 
 function removeMarkers(){
@@ -159,17 +164,31 @@ function removeMarkers(){
 		}
 	});
 }
+
 $.getJSON("./data/liste-des-cafes-a-un-euro.geojson", function(bars) {
 	data.bars = bars;
 	data.bars.markers = [];
+	data.bars.ages = [18, 35]
+
+	$.getJSON("./data/les_salles_de_cinemas_en_ile-de-france.geojson", function(cinemas) {
+		data.cinemas = cinemas;
+		data.cinemas.markers = [];
+		data.cinemas.ages = [7, 77]
+	});
+
+	$.getJSON("./data/manege_et_jeux.geojson", function(maneges) {
+		data.maneges = maneges;
+		data.maneges.markers = [];
+		data.maneges.ages = [7, 10]
+	});
 }).done(function(){
 	function get_data(layer){
 		var element_id = 0;
 		setTimeout(function(){
 			$.each(data, function( dataSet ){
 				for( var i = 0 in data[dataSet].features ){
-					if ( data[dataSet].features[i].properties.lat_lon && pointIsInPoly( data[dataSet].features[i].properties.lat_lon, layer.feature.geometry.coordinates[0][0]) ){
-						data[dataSet].markers[ element_id ] = L.marker(data[dataSet].features[i].properties.lat_lon);
+					if ( data[dataSet].features[i].geometry && pointIsInPoly( data[dataSet].features[i].geometry.coordinates, layer.feature.geometry.coordinates[0][0]) ){
+						data[dataSet].markers[ element_id ] = L.marker([data[dataSet].features[i].geometry.coordinates[1], data[dataSet].features[i].geometry.coordinates[0]]);
 						map.addLayer(data[dataSet].markers[ element_id ]);
 						element_id ++;
 					};
@@ -216,7 +235,7 @@ $.getJSON("./data/liste-des-cafes-a-un-euro.geojson", function(bars) {
 		function onEachFeature(feature, layer) {
 			$.each(data, function( dataSet ){
 				for( var i = 0 in data[dataSet].features){
-					if ( data[dataSet].features[i].properties.lat_lon && pointIsInPoly( data[dataSet].features[i].properties.lat_lon, layer.feature.geometry.coordinates[0][0]) ){	
+					if ( data[dataSet].features[i].geometry && data[dataSet].ages[0] <= age_user && data[dataSet].ages[1] >= age_user && pointIsInPoly( data[dataSet].features[i].geometry.coordinates, layer.feature.geometry.coordinates[0][0]) ){	
 						parArrondissement[number].elements ++;
 					}
 				}
